615088b80f13227e927016655e91046a17f345fd
euberdeveloper <euberdeveloper@gmail.com>
2022-06-25 19:49:12 +0200

    fix: remove extra slash
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 async function main() {
-    const httpUrl = 'https://www.stronzo.it/';
+    const httpUrl = 'https://github.com/origreen/static-assets/raw/main/white_cabbage/';
     const framesPath = path.join(__dirname, 'frames');
     const files = (await fs.promises.readdir(framesPath)).filter(file => !file.endsWith('.js'));
     const jsonResult: { date: Date, url: string }[] = [];
@@ -11,7 +11,7 @@ async function main() {
     for (const file of files) {
         await fs.promises.rename(path.join(framesPath, file), path.join(framesPath, `${currentDate.toISOString().slice(0, 10)}.jpg`));
         currentDate.setTime(currentDate.getTime() + 2 * 24 * 60 * 60 * 1000);
-        jsonResult.push({ date: currentDate, url: `${httpUrl}/frames/${file}` });
+        jsonResult.push({ date: currentDate, url: `${httpUrl}${file}` });
     }
 
     await fs.promises.writeFile(path.join(__dirname, 'index.json'), JSON.stringify(jsonResult));
