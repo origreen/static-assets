f4d98da866cd3da564cbefdb318c4b45836049f0
euberdeveloper <euberdeveloper@gmail.com>
2022-06-25 19:37:54 +0200

    feat: add frames
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

---
+++ main.ts
@@ -0,0 +1,19 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+async function main() {
+    const httpUrl = 'https://www.stronzo.it/';
+    const framesPath = path.join(__dirname, 'frames');
+    const files = (await fs.promises.readdir(framesPath)).filter(file => !file.endsWith('.js'));
+    const jsonResult: { date: Date, url: string }[] = [];
+
+    let currentDate = new Date('2020-03-01');
+    for (const file of files) {
+        await fs.promises.rename(path.join(framesPath, file), path.join(framesPath, `${currentDate.toISOString().slice(0, 10)}.js`));
+        currentDate.setTime(currentDate.getTime() + 2 * 24 * 60 * 60 * 1000);
+        jsonResult.push({ date: currentDate, url: `${httpUrl}/frames/${file}` });
+    }
+
+    await fs.promises.writeFile(path.join(__dirname, 'index.json'), JSON.stringify(jsonResult));
+}
+main();
